42582004b420e85495ab2c832d5be07c5606464d
Sofa <squishysparrow7.7@gmail.com>
2023-06-20 16:48:41 +1200

    Tab settings

--- lua/coolio-vims/options.lua
+++ lua/coolio-vims/options.lua
@@ -1,2 +1,11 @@
 vim.o.number = true
 vim.o.relativenumber = true
+
+local tabsize = 4
+vim.o.tabstop = tabsize
+vim.o.softtabstop = tabsize
+vim.o.shiftwidth = tabsize
+vim.o.smarttab = true
+vim.o.smartindent = true
+
+
